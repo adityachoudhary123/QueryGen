0443b988089356f88362ee973a45fdb85c1ec944
Vivek Kumar Gupta <vivekkumargupta326@gmail.com>
2024-12-01 12:10:30 +0530

    Improved prompt

--- src/QueryGen.js
+++ src/QueryGen.js
@@ -8,7 +8,7 @@ function QueryGen() {
   const [output, setOutput] = useState('');
   const { GoogleGenerativeAI } = require("@google/generative-ai");
   const genAI = new GoogleGenerativeAI("AIzaSyBOAMVSC4E-_BOVRwdAYhEhB7qGCciPFwI");
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
   
   
  
@@ -17,7 +17,16 @@ function QueryGen() {
     e.preventDefault();
     // Simulate query processing logic
 
-    const prompt = 'Write only sql query nothing else for, Database = ' + sqlType + ', and query = ' + query +  ', for Schema = ' + schema;
+    const prompt = 'You are a database expert who provides apt SQL queries for the information requested by the user.\n'+
+    'You are provided with 3 things. 1. A database type eg. MySQL, MSSQL or Postgres after the keyword `type`\n'+
+    '2. A query representing the information requested by the user after the keyword `query`\n'+
+    '3. The schema of the database in the form of DDL commands after the keyword `schema`\n'+
+    'If the schema provided does not represent any DDL command, you should give an error response\n'+
+    'You might have to join multiple tables to get the job done. Write the most efficient query to retrieve the information requested\n'+
+    'For any offensive data requested, return an error response\n'+
+    '`type`' + sqlType + ', \n' +
+    '`query`' + query + ', \n' +
+    '`schema`' + schema;
 
     const result = (await model.generateContent(prompt)).response.text();
     const refinedresult = result.replace('sql', '').replaceAll('```', '');
